c6cff53c4f185ea40681356881ef002acd176081
univDarko <darmunoz21@alumnos.utalca.cl>
2024-04-11 00:54:57 -0400

    Arreglo rapido
    No utilizé los persistentfooterbuttons como fue instruido, pero ahora si, y funcionan.

--- lab3_dispmovdarko/lib/main.dart
+++ lab3_dispmovdarko/lib/main.dart
@@ -127,28 +127,26 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      persistentFooterButtons: <Widget>[
+        IconButton(
+          onPressed: _incrementCounter, 
+          icon: const Icon(Icons.add)),
+        const SizedBox(width: 5),
+        IconButton(
+          onPressed: _decrementCounter, 
+          icon: const Icon(Icons.remove)),
+      ],
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(left: 30),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            FloatingActionButton(
-              onPressed: _decrementCounter,
-              tooltip: 'Decrement',
-              child: const Icon(Icons.remove),
-            ),
             Expanded(child: Container()),
             FloatingActionButton(
               onPressed: _resetCounter,
               tooltip: 'Reset',
               child: const Icon(Icons.replay_outlined),
             ),
-            Expanded(child: Container()),
-            FloatingActionButton(
-              onPressed: _incrementCounter,
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            )
           ],
         ),
       )
