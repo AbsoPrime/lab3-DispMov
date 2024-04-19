c773f57414335d9f16f3fe5396f4285a57d599e8
univDarko <darmunoz21@alumnos.utalca.cl>
2024-04-19 16:05:41 -0400

    parte 1 lab 5
    De a poco, por partes. Se eliminaron los comentarios innecesarios

--- lab3_dispmovdarko/test/widget_test.dart
+++ lab3_dispmovdarko/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lab3_dispmovdarko/main.dart';
+import 'package:lab3_dispmovdarko/pages/pantalla.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MiPantalla());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
