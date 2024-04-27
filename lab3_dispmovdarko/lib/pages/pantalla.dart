711fa171e32060d2847988b395e890d33ed067ef
univDarko <darmunoz21@alumnos.utalca.cl>
2024-04-27 15:35:34 -0400

    Lab5 Terminado
    Tiene todo

--- lab3_dispmovdarko/lib/pages/pantalla.dart
+++ lab3_dispmovdarko/lib/pages/pantalla.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MiPantalla extends StatelessWidget{
 
@@ -34,10 +35,21 @@ class _MiPantalla extends State<MyHomePage> {
   final String undoIcon = 'assets/icons/redoIcon2.svg';
   final String plusIcon = 'assets/icons/plus.svg';
   final String minusIcon = 'assets/icons/minus.svg';
+  final String playIcon = 'assets/icons/playIcon.svg';
+
+  final String happyIcon = 'assets/icons/happyFace.svg';
+  final String sadIcon = 'assets/icons/sadFace.svg';
+  final String neutralIcon = 'assets/icons/neutralFace.svg';
+  String statusIcon = 'assets/icons/neutralFace.svg';
+
+  String message = "Ingrese un número luego pulse el botón Play";
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+      if (_counter > 15){
+
+      }
     });
   }
 
@@ -51,23 +63,86 @@ class _MiPantalla extends State<MyHomePage> {
     setState(() {
       _counter = 0;
     });
+  }
 
-    bool useMaterial3 = true;
+  void _update(){
+    setState(() {
+      if (_counter == 10){
+        message = "Derrota";
+        statusIcon = sadIcon;
+      }
+      else if (_counter == 15){
+        message = "Victoria";
+        statusIcon = happyIcon;
+      }
+      else{
+        message = "Ingrese un número luego pulse el botón Play";
+        statusIcon = neutralIcon;
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context){
-    return  const Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.album),
-              title: Text('The Enchanted Nightingale'),
-              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: const Text("Una demo de Darko Muñoz"),
+        ),
+        body: Center(
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                //Icono
+                ListTile(
+                  title: SvgPicture.asset(statusIcon)
+                ),
+                //Contador
+                ListTile(
+                  title: Text('$_counter',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                        textAlign: TextAlign.center,
+                        textScaler: const TextScaler.linear(2)),
+                ),
+                //Mensaje
+                ListTile(
+                  title: Text(message, 
+                    textAlign: TextAlign.center, 
+                    textScaler: const TextScaler.linear(2)),
+                ),
+                //Botones
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                      icon: SvgPicture.asset(plusIcon,
+                            width: 35,
+                            height: 35,),
+                      onPressed: () {_incrementCounter();},
+                    ),
+                    const SizedBox(width: 8),
+                    IconButton(
+                      icon: SvgPicture.asset(playIcon,
+                            width: 35,
+                            height: 35,),
+                      onPressed: () {_update();
+                                      _resetCounter();},
+                    ),
+                    const SizedBox(width: 8),
+                    IconButton(
+                      icon: SvgPicture.asset(minusIcon,
+                            width: 35,
+                            height: 35,),
+                      onPressed: () {_decrementCounter();},
+                    ),
+                    const SizedBox(width: 8),
+                  ],
+                ),
+              ]
             )
-          ]
+          )
         )
       )
     );
